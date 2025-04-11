1b42a8488ed9e00d22428665d1b33d6fff12fa4b
enwadiogbu <emekanwadiogbu.jr@me.com>
2025-04-10 22:35:48 -0700

    check passwords for multiple passwords using loops

--- script.js
+++ script.js
@@ -16,13 +16,14 @@ var newsfeed = [
     {
         username: 'Sally',
         timeline: 'Javascript is so cool!'
+    },
+    {
+        username: 'Mitch',
+        timeline: 'Javascript is pretyy cool!'
     }
 ];
 
-var userNamePrompt = prompt("What's your username?");
-var passwordPrompt = prompt("What's your password?");
-
-function signIn(user, pass) {
+function signIn(username, password) {
     if(user === database[0].username && pass === database[0].password) {
         console.log(newsfeed);
     }
@@ -30,5 +31,8 @@ function signIn(user, pass) {
         alert('Sorry, wrong username and password');
     }
 }
+var userNamePrompt = prompt("What\'s your username?");
+var passwordPrompt = prompt("What\'s your password?");
+
 signIn(userNamePrompt, passwordPrompt);
         // console.log(userNamePrompt);
